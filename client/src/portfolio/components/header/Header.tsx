0c5ec5688fd431b2fbb71c82381f826e19141248
Dmytro Kenev <sinteenage@gmail.com>
2023-03-03 17:16:36 +0200

    refactoring waves

--- client/src/portfolio/components/header/Header.tsx
+++ client/src/portfolio/components/header/Header.tsx
@@ -3,14 +3,14 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import './header.css';
 
 import { Canvas } from '../canvas/Canvas';
-import { drawWave } from '../../utils/drawWave';
 import { Socials } from './Socials';
+import { useWave } from '../../hooks/useWave';
+import { waveOne, waveThree, waveTwo } from '../../types';
 
 export const Header: React.FC = () => {
 
     const widthRef = useRef(window.innerWidth);
     const height = 600;
-    const phase = useRef(0);
 
     const onChangeWidth = useCallback(() => {
         widthRef.current = window.innerWidth;
@@ -23,62 +23,17 @@ export const Header: React.FC = () => {
         };
     }, [onChangeWidth]);
 
-    const drawShineWaveOne = useCallback((context: CanvasRenderingContext2D) => {
-
-        drawWave(
-            context, 
-            widthRef.current,
-            height,
-            0.003,
-            phase.current,
-            'rgba(0, 36, 72, 0.3)',
-            0.07,
-            0.1,
-        );
-
-        phase.current += 0.005;
-
-    }, []);
-
-    const drawShineWaveTwo = useCallback((context: CanvasRenderingContext2D) => {
-
-        drawWave(
-            context, 
-            widthRef.current, 
-            height, 
-            0.003,
-            phase.current + 5.4,
-            'rgba(0, 36, 72, 0.4)',
-            0.07,
-            0.1,
-        );
-
-        phase.current += 0.005;
-
-    }, []);
-
-    const drawShineWaveThree = useCallback((context: CanvasRenderingContext2D) => {
-
-        drawWave(
-            context, 
-            widthRef.current, 
-            height, 
-            0.003,
-            phase.current + 4.3,
-            'rgba(0, 36, 72, 0.5)',
-            0.07,
-            0.1,
-        );
-
-        phase.current += 0.005;
-
-    }, []);
+    const waves = [
+        useWave(widthRef, height, waveThree), 
+        useWave(widthRef, height, waveTwo), 
+        useWave(widthRef, height, waveOne)
+    ];
 
     return (
         <header id='home'>
-            <Canvas height={height} draw={drawShineWaveThree}/>
-            <Canvas height={height} draw={drawShineWaveTwo}/>
-            <Canvas height={height} draw={drawShineWaveOne}/>
+            {waves.map((item, index) => {
+                return <Canvas key={index} height={height} draw={item}/>
+            })}
             <div className="container header__container">
                 <h3 className="header__h3">Frontend Developer</h3>
                 <div className='header__title'>
---
+++ client/src/portfolio/hooks/useWave.ts
@@ -0,0 +1,32 @@
+import { MutableRefObject, useCallback, useRef } from "react";
+import { WaveProps } from "../types";
+
+import { drawWave } from "../utils/drawWave";
+
+export function useWave(
+        widthRef: MutableRefObject<number>, 
+        height: number,
+        waveProps: WaveProps
+    ): (context: CanvasRenderingContext2D) => void {
+    const phaseRef = useRef(0);
+    const { freqancy, phaseCorection, color, amplitude, angle } = waveProps;
+
+    return useCallback((
+        context: CanvasRenderingContext2D
+        ) => {
+
+        drawWave(
+            context, 
+            widthRef.current,
+            height,
+            freqancy,
+            phaseRef.current + phaseCorection,
+            color,
+            amplitude,
+            angle,
+        );
+
+        phaseRef.current += 0.008;
+
+    }, [widthRef, height, freqancy, phaseCorection, color, amplitude, angle]);
+};
