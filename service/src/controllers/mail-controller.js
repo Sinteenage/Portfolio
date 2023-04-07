926bc7943ffbbb407b9cadd1bf2fbe9feef4b090
Dmytro Kenev <sinteenage@gmail.com>
2023-04-07 19:18:18 +0300

    mail send to main post

--- service/src/controllers/mail-controller.js
+++ service/src/controllers/mail-controller.js
@@ -1,7 +1,30 @@
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+
+dotenv.config();
+
 const sendMail = (req, res) => {
-    console.log('message: ' + req.body.message);
     
-    res.status(200).json({mail: 'sended'});
+    const transporter = nodemailer.createTransport({
+        host: 'mail.dkenev.com',
+        port: process.env.POST_PORT,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.MAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: `${req.body.email}`,
+        to: 'main@dkenev.com',
+        subject: `${req.body.name}`,
+        text: `${req.body.message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error) => {
+        res.json({mail: 'sended', error});
+    });
 };
 
 module.exports = sendMail;
